e4e6cfebb4616a55e35424a91d2a018d76672614
niemaattarian <G00346901@gmit.ie>
2020-01-23 18:29:22 +0000

    Created header and footer in main-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import {RouteModule} from './route/route.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouteModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]

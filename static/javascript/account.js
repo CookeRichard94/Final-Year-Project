7d150bcb6496830ec848dce2f0dfd6f797d938c0
niemaattarian <G00346901@gmit.ie>
2020-03-26 23:09:21 +0000

    Completed pop up page for login and register

---
+++ static/javascript/account.js
@@ -0,0 +1,9 @@
+// Sign-up
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}

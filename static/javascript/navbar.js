a9342391669b0409f6a8c7a2d0ae4b1db04c93ae
niemaattarian <G00346901@gmit.ie>
2020-05-06 02:14:58 +0100

    Started introduction and context chapters

--- static/javascript/navbar.js
+++ static/javascript/navbar.js
@@ -31,23 +31,3 @@ function backToTop() {
     window.scrollTo(0, 0);
 }
 
-
-// Sign-up
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-// Login
-var modal = document.getElementById('id02');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
